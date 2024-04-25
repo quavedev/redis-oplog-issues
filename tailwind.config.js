c23c28ec9cb95baa443e585466413eb6b6a67a9f
filipenevola <filipe@quave.com.br>
2024-04-25 17:45:44 -0400

    Adds prettier.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./imports/ui/**/*.{js,jsx,ts,tsx}",
-    "./client/*.html",
-  ],
+  content: ['./imports/ui/**/*.{js,jsx,ts,tsx}', './client/*.html'],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
